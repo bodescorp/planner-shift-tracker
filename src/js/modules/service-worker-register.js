c08eb0a103e1e11f5306253ae15b85a0a6fd9209
Glaymar <glaymar2010@gmail.com>
2025-12-29 16:00:08 -0300

    fix: Adicionar escopo e tratamento de erro ao registrar o Service Worker

--- src/js/modules/service-worker-register.js
+++ src/js/modules/service-worker-register.js
@@ -5,7 +5,9 @@
 export function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./src/js/sw.js')
+            navigator.serviceWorker.register('./src/js/sw.js', { scope: '/' })
+                .then(reg => console.log('Service Worker registrado:', reg.scope))
+                .catch(err => console.error('Erro ao registrar SW:', err));
         });
     }
 }
