f10fa502ae0aae8011257d3de099435df42401fb
Glaymar <glaymar2010@gmail.com>
2025-12-29 15:44:31 -0300

    fix: Corrigir caminho do service worker e ajustar meta tag para compatibilidade

--- src/js/modules/service-worker-register.js
+++ src/js/modules/service-worker-register.js
@@ -5,7 +5,7 @@
 export function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/src/js/sw.js')
+            navigator.serviceWorker.register('./src/js/sw.js')
         });
     }
 }
