c4b54b13843c62ca10e4784767f21b17ae1b67a0
Glaymar <glaymar2010@gmail.com>
2025-12-29 16:01:41 -0300

    fix: Atualizar registro do Service Worker e restaurar arquivo de configuração

--- src/js/modules/service-worker-register.js
+++ src/js/modules/service-worker-register.js
@@ -5,7 +5,7 @@
 export function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./src/js/sw.js', { scope: '/' })
+            navigator.serviceWorker.register('./sw.js')
                 .then(reg => console.log('Service Worker registrado:', reg.scope))
                 .catch(err => console.error('Erro ao registrar SW:', err));
         });
